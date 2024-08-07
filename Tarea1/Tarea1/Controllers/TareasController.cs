5dc1167e72a0ea3d47b0b7706aa23e0be523673f
FacundoMolina04 <129466281+FacundoMolina04@users.noreply.github.com>
2024-08-07 19:23:43 -0300

    Add files via upload
    Capturas de Swagger en el PDF adjuntado.

---
+++ Tarea1/Tarea1/Controllers/TareasController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Tarea1.Models;
+
+
+namespace Tarea1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TareasController : Controller
+    {
+        private readonly ILogger<TareasController> _logger;
+
+        private IList<Tarea> _tareasList;
+
+        public TareasController(ILogger<TareasController> logger)
+        {
+            this._logger = logger;
+
+            this._tareasList = new List<Tarea>();
+
+            this._tareasList.Add(new Tarea(1, "TestTarea1", "Picar1", 23, "Debugear", DateTime.Now));
+            this._tareasList.Add(new Tarea(2, "TestTarea2", "Picar2", 24, "Debugear2", DateTime.Parse("2024-08-15 10:30:00")));
+           
+
+
+        }
+                
+        [HttpGet]
+        public ActionResult<IList<Tarea>> GetAll()
+        {
+            _logger.LogInformation("Retorno lista de tareas");
+            return Ok(_tareasList.ToList());
+        }
+
+
+        [HttpGet]
+        [Route("{id}")]
+        public ActionResult<Tarea> GetById(int id)
+        {
+            _logger.LogInformation($"Retorno tarea con id: {id}");
+            return Ok(_tareasList[id]);
+        }
+
+        [HttpPost]
+        public ActionResult CrearTarea([FromBody] Tarea tarea)
+        {
+            _logger.LogInformation($"Nueva tarea con id: {tarea.Id}");
+            this._tareasList.Add(tarea);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteById(int id)
+        {
+            _logger.LogInformation($"Borrar tarea con id: {id}");
+            this._tareasList.RemoveAt(id);
+            return Ok();
+        }
+    }
+}
---
+++ Tarea1/Tarea1/Models/Tarea.cs
@@ -0,0 +1,61 @@
+﻿namespace Tarea1.Models
+{
+    public class Tarea
+    {
+        private int _id;
+        public int Id
+        {
+            set { _id = value; }
+            get { return _id; }
+        }
+
+        private string _nombre;
+        public string Nombre
+        {
+            set { _nombre = value; }
+            get { return _nombre; }
+        }
+
+        private string _descripcion;
+        public string Descripcion
+        {
+            set { _descripcion = value; }
+            get { return _descripcion; }
+        }
+
+        private int _duracionHoras;
+        public int DuracionHoras
+        {
+            set { _duracionHoras = value; }
+            get { return _duracionHoras; }
+        }
+
+        private string _responsable;
+        public string Responsable
+        {
+            set { _responsable = value; }
+            get { return _responsable; }
+        }
+
+        private DateTime _fecha;
+        public DateTime Fecha
+        {
+            set { _fecha = value; }
+            get { return _fecha; }
+        }
+
+        public Tarea(int id, string nombre, string descripcion, int duracionHoras, string responsable, DateTime fecha)
+        {
+            this._id = id;
+            this._nombre = nombre;
+            this._descripcion = descripcion;
+            this._duracionHoras = duracionHoras;
+            this._responsable = responsable;
+            this._fecha = fecha;
+        }
+
+        public Tarea()
+        {
+        }
+    }
+}
